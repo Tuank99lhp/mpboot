b4a879a11d67a8a6ccf20b70aee31efd7f8493f6
tuanvq <vutuanchuyentinlhp@gmail.com>
2025-04-23 10:52:11 +0700

    fix log

--- strictconsensusmerge.cpp
+++ strictconsensusmerge.cpp
@@ -330,7 +330,7 @@ GeneTree *StrictConsensusMerge::getSCMTree() {
         sort(treePairsScore.back().begin(), treePairsScore.back().end());
     }
 
-    cout << "SCM: Merged SCM trees successfully" << endl;
+    cout << "SCM: Merging gene trees by SCM successfully" << endl;
     // for (int i = 0; i < trees.size() - 1; ++i) {
     //     assert(trees[i] == NULL);
     // }
